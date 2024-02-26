9a0f23e518c5319619cf76fb8750df036f8f355f
DeCaroLorenzo <de.caro.lorenzo.2021i@bankitatabanya.hu>
2024-02-26 14:12:11 +0100

    Localstorage js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,75 @@
+function hasStorageSupport() {
+    return typeof(Storage) !== "undefined";
+}
+
+/*
+function setStorage(name, value) {
+    if (hasStorageSupport()) {
+
+    }
+}
+*/
+
+function setBookmark(bookmark) {
+    if (hasStorageSupport()) {
+        var bookmarks = localStorage.getItem("HANGOSKONYV_BOOKMARKS");
+        bookmarks = bookmarks ? JSON.parse(bookmarks) : [];
+        var removed = false
+        for (i in bookmarks) {
+            if (bookmarks[i].name == bookmark.name) {
+                bookmarks.splice(i, 1);
+                removed = true
+            }
+        }
+        if (!removed) {
+            bookmarks.push(bookmark);
+        }
+        console.log(bookmarks)
+        localStorage.setItem("HANGOSKONYV_BOOKMARKS", JSON.stringify(bookmarks));
+
+        return removed
+    }
+}
+
+function addToCart() {
+    alert("Add To Cart");
+}
+
+function addBookmark() {
+    var bookmark = {
+        name: document.querySelector("#audiobookName").innerHTML,
+        img: document.querySelector("#audiobookImg").src,
+        source: window.location.href
+    };
+
+    var removed = setBookmark(bookmark)
+    if (!removed) {
+        document.querySelector(".addBookmark").style.display = "none";
+        document.querySelector(".removeBookmark").style.display = "block";
+    } else {
+        document.querySelector(".addBookmark").style.display = "block";
+        document.querySelector(".removeBookmark").style.display = "none";
+    }
+}
+
+// localStorage.setItem("HANGOSKONYV_BOOKMARKS", "");
+// Load storage
+if (hasStorageSupport()) {
+    var bookmarks = localStorage.getItem("HANGOSKONYV_BOOKMARKS");
+    bookmarks = bookmarks ? JSON.parse(bookmarks) : [];
+    console.log(bookmarks)
+
+    var bookmarkContainer = document.querySelector(".bookmarkContainer")
+    for (i in bookmarks) {
+        if (document.querySelector("#audiobookName").innerHTML == bookmarks[i].name) {
+            document.querySelector(".addBookmark").style.display = "none";
+            document.querySelector(".removeBookmark").style.display = "block";
+        }
+
+        bookmarkContainer.innerHTML += '<a href="'+bookmarks[i].source+'"><article class="row">    <div class="col-4"><img class="w-100" src="'+bookmarks[i].img+'" alt=""></div>    <div class="col-8 my-auto">        <p>'+bookmarks[i].name+'</p>    </div></article></a>'
+    }
+}
+
 // Bootstrap validáció
 (() => {
   'use strict'
