9aed76b0fae1de542f3d2444e6d63cad06366ac7
VojcsekTunde <94844199+VojcsekTunde@users.noreply.github.com>
2024-02-26 14:11:12 +0100

    index javítás

--- js/script.js
+++ js/script.js
@@ -15,3 +15,7 @@
     }, false)
   })
 })()
+
+var SearchBar = document.getElementById("SearchBar");
+
+var audiosTitle=["Fantasy-1","Fantasy-2","Fantasy-3","Fantasy-4","Gyerek-1","Gyerek-2","Gyerek-3","Gyerek-4","Ifjúsági-1","Ifjúsági-2","Ifjúsági-3","Ifjúsági-4","Szépirodalmi-1","Szépirodalmi-2","Szépirodalmi-3","Szépirodalmi-4","Ismeretterjesztő-1","Ismeretterjesztő-2","Ismeretterjesztő-3","Ismeretterjesztő-4"];
