af8cb18462f52502438ec523e0ab36afc384af67
VojcsekTunde <94844199+VojcsekTunde@users.noreply.github.com>
2024-03-04 14:11:07 +0100

    validáció elkezdése

--- js/script.js
+++ js/script.js
@@ -351,3 +351,10 @@ function submitPurchase() {
       }
     }
   }
+
+  function isNumber(event){
+    var charCode = (event.which) ? event.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+    }
